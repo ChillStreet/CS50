b56beecdeba5ddfd8bdc17891618f1d989768da2
ChillStreet <chillstreetwest1@gmail.com>
2021-01-25 10:08:11 +0000

    Update Bigaddition.c

--- Bigaddition.c
+++ Bigaddition.c
@@ -1,1 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
 
+int main(void)
+{
+    long x = get_long("Input 1st number:");
+
+    long y = get_long("Input second number:");
+
+    printf("%li\n" , x + y);
+}
