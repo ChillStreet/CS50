e3b3dadc01f2bb40f1800a89ecf846ae92125576
ChillStreet <chillstreetwest1@gmail.com>
2021-02-24 14:29:55 +0000

    Cash.c
    This method works. It stores the amount of coins in individual variables, then adds them up for the total.

--- Cash.c
+++ Cash.c
@@ -10,7 +10,7 @@ int main ()
 
     do
     {
-        dollars = get_float("Change owed:");
+        dollars = get_float("Change owed: ");
     }
     while (dollars < 0);
 
@@ -18,6 +18,44 @@ int main ()
 
 int cents = round(dollars * 100);
 
+//Divide by 25 to find number of Quarters
 
-    printf("%i\n", cents);
+int quarters = cents / 25;
+
+//Get remainder of cents / 25
+
+int remaining = cents % 25;
+
+//Dimes = remaining / 10
+
+int dimes = remaining / 10;
+
+//Get remainder of remaining / 10
+
+int remaining2 = remaining % 10;
+
+//Nickels = remaining2 / 5
+
+int nickels = remaining2 / 5;
+
+//Get remainder of remaining2 / 5
+
+int remaining3 = remaining2 % 5;
+
+//Pennies = remaining3
+
+int pennies = remaining3;
+
+//Total number of coins
+
+int coins = quarters + dimes + nickels + pennies;
+
+//Print all coins to check values are correct
+
+    printf("Quarters: %i\n", quarters);
+    printf("Dimes: %i\n", dimes);
+    printf("Nickels: %i\n", nickels);
+    printf("Pennies: %i\n", pennies);
+    printf("%i\n", coins);
+
 }
