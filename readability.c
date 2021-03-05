a6d26bd503675460ec7c50b443523198ff22d873
ChillStreet <chillstreetwest1@gmail.com>
2021-03-05 13:44:25 +0000

    Update readability.c
    Modify readability.c so that, instead of printing out the literal text itself, it instead prints out a count of the number of letters in the text.
    
    Uses a function called count_letters. This function uses isalpha to determine whether a character is alphabetical. 
    If character is alphabetical, 1 is added to letter count. 
    If not alphabetical, 0 is added to letter count.
    
    'count' is then returned and assigned to 'letters', printing out the number of letters in the inputted text.
    
    The function returns an integer representing the number of letters within the inputted text and this is printed

--- readability.c
+++ readability.c
@@ -2,10 +2,39 @@
 #include <stdio.h>
 #include <math.h>
 #include <ctype.h>
+#include <string.h>
+
+int count_letters(string input);
 
 int main(void)
 {
+//Get text from user
     string input = get_string("Text: ");
 
-    printf("%s \n", input);
+//Calculate number of letters using count_letters function
+
+    int letters  = count_letters(input);
+
+    printf("%i letter(s)\n", letters);
+}
+
+int count_letters(string input)
+//Count letters in the string
+{
+    int total = strlen(input);
+    int count = 0;
+
+    for (int i = 0; i < total; i++)
+    {
+        if (isalpha(input[i]))
+        {
+            count++;
+        }
+        else
+        {
+            count += 0;
+        }
+
+    }
+    return count;
 }
