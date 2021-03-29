26fa0d25dd7e324a3747eb164c71f92cbb033374
chillstreet <chillstreetwest1@gmail.com>
2021-03-29 17:54:37 +0100

    Caesar.c
    Modify caesar.c in such a way that: if the user provides exactly one command-line argument, it prints Success; if the user provides no command-line arguments, or two or more, it prints Usage: ./caesar key.

---
+++ caesar.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main (int argc, string argv[])
+{
+    if (argc == 2)
+    {
+        printf("Success\n");
+    }
+    else
+    {
+        printf("Usage: ./caesar key\n");
+    }
+}
