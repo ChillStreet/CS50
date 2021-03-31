797d7589129f7e61d955a19ace35c7b70c5c16b1
chillstreet <chillstreetwest1@gmail.com>
2021-04-01 00:43:23 +0100

    Update caesar.c
    Now ignores spaces, punctuation. Added more comments.

--- caesar.c
+++ caesar.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main (int argc, string argv[])
+int main(int argc, string argv[])
 {
     if (argc == 2)
 //If only 2 arguments are input, iterate through second argument to check for digits
@@ -13,7 +13,7 @@ int main (int argc, string argv[])
         for (int i = 0; i < strlen(key); i++)
         {
 //If any character is not a digit, return an error
-            if(isdigit(key[i]) == 0)
+            if (isdigit(key[i]) == 0)
             {
                 printf("Usage: ./caesar key\n");
                 return 1;
@@ -30,26 +30,33 @@ int main (int argc, string argv[])
 
 //If inputted characters are all digits, convert 2nd argument from string to integer
 
-     int keyint = atoi(argv[1]);
+    int keyint = atoi(argv[1]);
 
 //Get plaintext from user
 
-     string p = get_string("plaintext: ");
+    string p = get_string("plaintext: ");
 
-     printf("ciphertext: ");
+    printf("ciphertext: ");
 
 //Use a for loop to iterate through plaintext
 
-    for(int j = 0; j < strlen(p); j++)
+    for (int j = 0; j < strlen(p); j++)
     {
+//If input is lowercase print calculated ciphertext
         if (islower(p[j]))
         {
             printf("%c", ((p[j] + keyint - 'a') % 26 + 'a'));
         }
+//If input is uppercase print calculated ciphertext
         else if (isupper(p[j]))
         {
-            printf("%c", ((p[j]+ keyint - 'A') % 26 + 'A'));
+            printf("%c", ((p[j] + keyint - 'A') % 26 + 'A'));
+        }
+//Leave spaces, punctuation as they are
+        else
+        {
+            printf("%c", p[j]);
         }
     }
-        printf("\n");
-}
+    printf("\n");
+    return 0;
