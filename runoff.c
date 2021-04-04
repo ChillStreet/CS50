7c2a4222c2401f48d927915b9eee4ad39b5388fc
chillstreet <chillstreetwest1@gmail.com>
2021-04-04 12:35:26 +0100

    Complete the vote function
    The function takes arguments voter, rank, and name. If name is a match for the name of a valid candidate, then you should update the global preferences array to indicate that the voter voter has that candidate as their rank preference (where 0 is the first preference, 1 is the second preference, etc.).
    
    If the preference is successfully recorded, the function should return true; the function should return false otherwise (if, for instance, name is not the name of one of the candidates).
    
    You may assume that no two candidates will have the same name.

--- runoff.c
+++ runoff.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max voters and candidates
 #define MAX_VOTERS 100
@@ -127,7 +128,17 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    // TODO
+// Iterate through candidates
+// Check name entered is stored in canidates array
+// If so, use i counter to indicate order of preference and return true
+    for (i = 0; i < candidate_count; i++)
+    {
+        if (!strcmp(candidates[i].name))
+        {
+            preferences[voter][rank]= i;
+            return true;
+        }
+    }
     return false;
 }
 
